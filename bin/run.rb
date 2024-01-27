0c2c95c8e66c9bf2d22dba41d0908efb179c1c8e
francesca sandford <fran6san@gmail.com>
2024-01-27 12:47:09 +0000

    feat: count bytes of file with test able to count bytes of a file - not yet implemented options spec passes for file in project or desktop but not ARGV

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,12 @@
 #!/usr/bin/env ruby
-
 # run the program from terminal using `./rwc`
+
+# implement method for determining bytes, not yet taking option into account
+
+_, file = ARGV
+
+def count_bytes(file_path)
+  File.size(file_path)
+end
+
+puts count_bytes(file)
---
+++ spec/run_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../bin/run'
+
+RSpec.describe 'run#count_bytes' do
+  it 'returns number of bytes in a file' do
+    result = count_bytes('./test.txt')
+    expect(result).to eq(342_190)
+  end
+end
+
+# test passes when provided file path in run.rb is not from ARGV but from
+# file in project or on desktop
