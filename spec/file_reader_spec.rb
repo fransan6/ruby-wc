cc1fa7b2eeec79e322dfb1db8cb4b82b6a8a2b5d
francesca sandford <fran6san@gmail.com>
2024-02-03 21:31:56 +0000

    feat(test): add for #count_words

--- spec/file_reader_spec.rb
+++ spec/file_reader_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/file_reader'
 # require 'file_reader'
 
-RSpec.describe FileReader do
+RSpec.describe FileReader do # rubocop:disable Metrics/BlockLength
   let(:file_reader) { FileReader.new }
 
   describe '#count_bytes' do
@@ -23,6 +23,17 @@ RSpec.describe FileReader do
 
     it 'returns the number of lines in a file' do
       expect(result).to eq(7145)
+    end
+  end
+
+  describe '#count_words' do
+    let(:result) { file_reader.count_words('./data/test.txt') }
+    it 'returns an integer' do
+      expect(result).to be_an_instance_of(Integer)
+    end
+
+    it 'returns the number of words in a file' do
+      expect(result).to eq(58_164)
     end
   end
 end
