6f8baa8f9165b5e01f9a8287fbc73d4369f6cc10
francesca sandford <fran6san@gmail.com>
2024-02-04 10:36:42 +0000

    feat(test): add for #count_characters

--- spec/file_reader_spec.rb
+++ spec/file_reader_spec.rb
@@ -1,11 +1,11 @@
 require_relative '../lib/file_reader'
-# require 'file_reader'
 
 RSpec.describe FileReader do # rubocop:disable Metrics/BlockLength
   let(:file_reader) { FileReader.new }
 
   describe '#count_bytes' do
     let(:result) { file_reader.count_bytes('./data/test.txt') }
+
     it 'returns an integer' do
       expect(result).to be_an_instance_of(Integer)
     end
@@ -17,6 +17,7 @@ RSpec.describe FileReader do # rubocop:disable Metrics/BlockLength
 
   describe '#count_lines' do
     let(:result) { file_reader.count_lines('./data/test.txt') }
+
     it 'returns an integer' do
       expect(result).to be_an_instance_of(Integer)
     end
@@ -28,12 +29,25 @@ RSpec.describe FileReader do # rubocop:disable Metrics/BlockLength
 
   describe '#count_words' do
     let(:result) { file_reader.count_words('./data/test.txt') }
+
     it 'returns an integer' do
       expect(result).to be_an_instance_of(Integer)
     end
 
     it 'returns the number of words in a file' do
       expect(result).to eq(58_164)
+    end
+  end
+
+  describe '#count_characters' do
+    let(:result) { file_reader.count_characters('./data/test.txt') }
+
+    it 'returns an integer' do
+      expect(result).to be_an_instance_of(Integer)
+    end
+
+    it 'returns the number of characters in a file' do
+      expect(result).to eq(339_292)
     end
   end
 end
